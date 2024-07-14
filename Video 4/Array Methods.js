ee2b1d245c24d4c41abcde2271745b2d3aeac0d2
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-14 10:44:45 +0530

    "Added code examples for various array methods in JavaScript, including push, pop, toString, and concat."

---
+++ Video 4/Array Methods.js
@@ -0,0 +1,39 @@
+//*********************Array methods ****************** */
+//1. push() method ===> (add to end) end data add kar deta hai
+//2. pop() method ===> (remove from end) end se data nikal deta hai
+//3. unshift() method ===> (add to start) start se data add kar deta
+//4. shift() method ===> (remove from start) start se data nikal deta hai
+//5. splice() method ===> (add/remove from middle) middle se data add kar deta
+//6. slice() method ===> (copy from middle) middle se data copy kar deta hai
+//7. concat() method ===> (merge two arrays) two arrays ko merge kar deta hai
+//8. join() method ===> (convert array to string) array ko string me convert kar d
+// toString() methode ====> (convert array to string) array ko string me convert kar d
+
+//***********************Push method ******************************* */
+// let foodItems = ["pateto", "apple", "litchi", "tamato"]
+// foodItems.push("mango")  // Hum isme multiple data add kar sakte hai
+// console.log(foodItems)
+//--------------------------------------------------------------
+//***********************Pop method ******************************* */
+// let foodItems = ["pateto", "apple", "litchi", "tamato"];
+// foodItems.pop();                // Hum isme deletd  item ko print kar sakte hai
+// let deletdItem = foodItems.pop();      //  the function is print delet valu
+// console.log(foodItems);
+// console.log(deletdItem);
+
+//----------------------------------------------------------------->
+//************************ toString  **********************/
+// let foodItems = ["pateto", "apple", "litchi", "tamato"];
+// console.log(foodItems.toString());                   // toString() method is convert array to string
+// let str = foodItems.toString();            // aise bhi hum chenge kar sakte hai
+
+// console.log(str);
+//-------------------------------------------------------------------------->
+// ************************* concat() methode jotint the to arrays*****************/
+
+// let marvel_heroes = ["thor", "spiderman", "ironman"];
+// let dc_heroes = ["superman", "batman", "wonderwoman"];
+// let all_heroes = marvel_heroes.concat(dc_heroes); // isme hum two arrays ko
+// join kar sakte hai
+// console.log(all_heroes);
+//------------------------------------------------------->
