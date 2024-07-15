fb0e3027f8e22266a3dfa3e5c4905145477c1fbd
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-15 09:16:47 +0530

    Arrow function

--- Video 5/index.js
+++ Video 5/index.js
@@ -1,1 +1,42 @@
-console.log("googleji")
+// console.log("googleji");
+
+//***************************function defination ******************** */
+// Exeample --------->
+// cerate a new funtion ----------->
+
+// function funName() {
+//   console.log("hello world");
+//   console.log("Hello Googleji");
+// }
+// // call the function ----------->
+
+// funName();
+//___________________________________________________________________________________
+// Hum function me value bhi paas kar sakte hai jo call ke time input dete hai
+// function me value pass karne ke liye function ke andar argument likhte hai
+
+//function myFunction(msg) {
+//parameter -> input  yaha multiple value use kar sakte hai
+//console.log(msg);
+//}
+//myFunction("I love JS"); //argument yaha hum multiple value daal sakte hai
+//_______________________________________________________________________________________________
+// function me value return karne ke liye return keyword use karte hai
+//__________________________________________________________________________________________
+// cerate a new function in to number sum ------>
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+// // call the function ----------->
+// sum(5, 5);
+//-------------------------------------------------------->
+// cerate a new function in to number sum ------>
+
+// function sum(a, b) {
+//   s = a + b;
+//   return s; // return ke baad koi aur code run  ya print nahi hoga
+// }
+// // call the function ----------->
+// let sm = sum(5, 5);
+// console.log(sm);
