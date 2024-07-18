4e57814a11a2d26feffe800171f3a69e5743902e
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-18 12:19:00 +0530

    New code

---
+++ Video 6/index.js
@@ -0,0 +1,28 @@
+// **************************This is rough area ****************************/
+// document.body.style.backgroundColor = "red";
+
+// let div = document.querySelector("#contenor");
+
+// div.innerText =
+//   div.innerText + `\nGoogleji` + `\n Hi my name is Gajendra vishwakarma`;
+
+// console.dir(div);
+
+//------------------------------------------------------------------------------>
+
+// ************************** Select with id  ****************************/
+// let div = document.querySelector("Myid");
+// console.log(div);
+// ************************** Select with class  ****************************/
+// let div = document.querySelector(".myclass");
+// console.log(div);
+// ************************** Select with tag  ****************************/
+// let div = document.querySelector("div");
+// console.log(div);
+// ************************** Select with all  ****************************/
+// let div = document.querySelector("div"); // Select only first elements---->
+//console.dir(div)
+//--------------------------------------------------->
+// let div = document.querySelectorAll("div");
+// console.log(div);
+//-------------------------------------------------------------------------------------->
---
+++ Video 6/index1.js
@@ -0,0 +1,8 @@
+// let heading = document.getElementById("heading");
+// console.dir(heading);
+
+// class name to track -------------------<>
+
+let myclass = document.getElementsByClassName("my-class");
+console.dir(myclass);
+console.log(myclass)
