06a3a3ec8638e4f6ab55582142ad699a8d4f2c27
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-18 21:20:57 +0530

    New file but i am faceing error

--- Video 6/index1.js
+++ Video 6/index1.js
@@ -3,6 +3,23 @@
 
 // class name to track -------------------<>
 
-let myclass = document.getElementsByClassName("my-class");
-console.dir(myclass);
-console.log(myclass)
+// let myclass = document.getElementsByClassName("my-class");
+// console.dir(myclass);
+// console.log(myclass)
+
+// ************************** Select with id  ****************************/
+
+// let div = document.getElementById("heading");
+// console.log(div);
+// ************************** Select with class  ****************************/
+
+// let div = document.getElementsByClassName("my-class");
+// console.log(div);
+// ************************** Select with tag  ****************************/
+// let para = document.getElementsByTagName("p");
+// para.innerText = "Googleji"
+// console.log(para);
+// console.log(para.innerText);
+//--------------------------------------------------------------------------------------------------->
+// let p = document.querySelector("p")
+// console.log(p)
