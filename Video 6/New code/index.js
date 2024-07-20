cabd0c94aa22c41037007dba638f50051f7fafb2
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-20 14:00:59 +0530

    New Exparements

---
+++ Video 6/New code/index.js
@@ -0,0 +1,34 @@
+console.log("Track By id ,class , Tag");
+
+// let p = document.querySelector("div");
+// console.log(p);
+// let cl = p.getAttribute("class"); //to get the attribute value
+// console.log(cl);
+
+// let id = document.getElementById("My-Id");
+// console.dir(id);
+// id.style.background = "red"
+// id.style.height = "600px"
+// id.style.width = "600px"
+// id.style.display = "flex"
+// id.style.alignItems = "center"
+// id.style.justifyContent = "center"
+// id.style.borderRadius = "20px"
+//__________________________________________________________________________________
+//   ****************************** Create Elements and Render elements ************************************/
+let D = document.querySelector("#My-Id");
+D.style.display = "flex";
+D.style.alignItems = "center";
+D.style.justifyContent = "center";
+D.style.borderRadius = "20px";
+D.style.flexDirection = "column";
+let New = document.createElement("div");
+New.innerText = "New div";
+New.setAttribute("id", "New-Id");
+New.style.borderRadius = "20px";
+D.prepend(New);
+console.log(New);
+//-------------------------------------------------------->
+// id.setclasslist("Id","New-Id")
+// let at = id.getAttribute("Id")
+// console.log(at);
