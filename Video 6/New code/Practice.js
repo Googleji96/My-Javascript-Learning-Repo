4cf4dd847bfd668908f67a5a20245c42da6a3f10
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-22 14:47:27 +0530

    New commit

---
+++ Video 6/New code/Practice.js
@@ -0,0 +1,16 @@
+// *********************Create a new button************************************************
+// let body = document.querySelector("body");
+// let Create = document.createElement("button");
+// Create.classList.add("My-Id")
+// Create.innerText = "Click Me!";
+// Create.style.background = "red";
+// Create.style.borderRadius = "10px";
+// Create.style.height = "100px";
+// Create.style.width = "200px";
+// Create.style.margin = "200px 500px";
+// Create.style.fontSize = "30px";
+// Create.style.color = "white";
+
+// body.prepend(Create);
+
+//////___________---------------------------------------------------->
--- Video 6/New code/index.js
+++ Video 6/New code/index.js
@@ -16,18 +16,18 @@ console.log("Track By id ,class , Tag");
 // id.style.borderRadius = "20px"
 //__________________________________________________________________________________
 //   ****************************** Create Elements and Render elements ************************************/
-let D = document.querySelector("#My-Id");
-D.style.display = "flex";
-D.style.alignItems = "center";
-D.style.justifyContent = "center";
-D.style.borderRadius = "20px";
-D.style.flexDirection = "column";
-let New = document.createElement("div");
-New.innerText = "New div";
-New.setAttribute("id", "New-Id");
-New.style.borderRadius = "20px";
-D.prepend(New);
-console.log(New);
+// let D = document.querySelector("#My-Id");
+// D.style.display = "flex";
+// D.style.alignItems = "center";
+// D.style.justifyContent = "center";
+// D.style.borderRadius = "20px";
+// D.style.flexDirection = "column";
+// let New = document.createElement("div");
+// New.innerText = "New div";
+// New.setAttribute("id", "New-Id");
+// New.style.borderRadius = "20px";
+// D.prepend(New);
+// console.log(New);
 //-------------------------------------------------------->
 // id.setclasslist("Id","New-Id")
 // let at = id.getAttribute("Id")
