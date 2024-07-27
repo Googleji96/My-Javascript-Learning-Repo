c9edd10057c106e4a1d37e0fc3f2a783253a272b
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-27 09:46:50 +0530

    New

---
+++ Video 11/index.js
@@ -0,0 +1,21 @@
+//********************Classes & Objects****************************/
+//___Semple objects ---->
+
+// const student = {
+//     fullName : "Googleji",
+//     age : 20,
+//     grade : 90
+// }
+// console.log(student)
+//___func in objects ----->
+// const student = {
+//   fullName: "Googleji",
+//   grade: 90,
+//   printGrade: function () {
+//     console.log("Grade = ", this.grade);
+//   },
+// };
+
+// console.log(student);
+
+//---------------------------------------------------------->
