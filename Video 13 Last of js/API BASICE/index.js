2928fd0e19540d1393bfaaf28d5425d1696363e6
Gajendra Vishwakarma <gajendravishwakarma004@gmail.com>
2024-07-29 13:22:40 +0530

    New

---
+++ Video 13 Last of js/API BASICE/index.js
@@ -0,0 +1,28 @@
+const URL = "https://cat-fact.herokuapp.com/facts";
+// const getFacts = async () => {
+//     console.log("geting data.......");
+//   let respose = await fetch(URL);
+//   console.log(respose); // isse mila data json formet me hota hai ja objects ki tarha dikhta hai
+// };
+//------------------------------------->
+
+// const getFacts = async () => {
+//     console.log("geting data.......");
+//   let respose = await fetch(URL);
+//   console.log(respose); // isse mila data json formet me hota hai ja objects ki tarha dikhta hai
+//   let data = await respose.json();
+//   console.log(data);
+// };
+//--------------------------------------------->
+// let fact = document.querySelector("#fact");
+// let btn = document.querySelector("#btn");
+
+// const getFacts = async () => {
+//   console.log("geting data.......");
+//   let respose = await fetch(URL);
+//   console.log(respose); // isse mila data json formet me hota hai ja objects ki tarha dikhta hai
+//   let data = await respose.json();
+//   fact.innerText = data[1].text;
+// };
+
+// btn.addEventListener("click", getFacts);
